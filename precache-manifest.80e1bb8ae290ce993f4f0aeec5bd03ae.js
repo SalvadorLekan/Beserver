5935f913d834cfc2ac00f4d232a8fa8ea07e5056
SalvadorLekan <66782276+SalvadorLekan@users.noreply.github.com>
2020-07-12 15:55:06 +0100

    Add files via upload

---
+++ precache-manifest.80e1bb8ae290ce993f4f0aeec5bd03ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88caf29b5c5e1ced505658186ad6d9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77de6299d7013480e66d",
+    "url": "/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "dcd1e82631af90f91f9e",
+    "url": "/static/css/main.11d0f69a.chunk.css"
+  },
+  {
+    "revision": "77de6299d7013480e66d",
+    "url": "/static/js/2.45eb860d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45eb860d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd1e82631af90f91f9e",
+    "url": "/static/js/main.e87d73f5.chunk.js"
+  },
+  {
+    "revision": "c4bf6c11e462db8a067a",
+    "url": "/static/js/runtime-main.09a197ad.js"
+  }
+]);
